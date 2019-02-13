fba65feebf8cc242a49a949f995e3ef1062f8d09
User <User@DEV-022783.OnBase.net>
2019-02-12 19:17:11 -0500

    Creates an abpout me page

--- Week02/Program.cs
+++ Week02/Program.cs
@@ -7,7 +7,24 @@ namespace Week02
         static void Main(string[] args)
         {
             // Create variables and do things with them!
-            
+            Console.WriteLine("hello WOrld");
+            string myName = "Arya";
+            int myAge = 32;
+            string favColor = "purple";
+            string myHobby = "soccer"; 
+            bool isOldEnoughToDrive = false;
+            double myHeight = 5.16; 
+            char firstInitial = 'A'; 
+
+            Console.WriteLine("My name is " + myName);
+           //Console.Writeline("My age is {0}", myAge);
+            Console.WriteLine("My age is " + myAge);
+            Console.WriteLine("My favorite color is " + favColor);
+            Console.WriteLine("My hobby is " + myHobby);
+            Console.WriteLine(" I am old enough to drive " + isOldEnoughToDrive);
+            Console.WriteLine("My height is " + myHeight);
+            Console.WriteLine("My first initial is " + firstInitial);
+
         }
     }
 }
