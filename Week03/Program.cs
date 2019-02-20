286ccc3e49859747fa2a588e3a2f7de22c6783ba
User <User@DEV-022783.OnBase.net>
2019-02-19 19:27:40 -0500

    Create a Program that asks for temperature input and output fun message

---
+++ Week03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Week03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Conditional Challenges 
+
+            Console.WriteLine("Pick a number 1-10");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 1){
+             Console.WriteLine("odd"); 
+            }
+        }
+    }
+
