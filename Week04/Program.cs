93bff135f1d06204027223b80f25a61aa271688c
User <User@DEV-022783.OnBase.net>
2019-02-26 19:23:44 -0500

    Did week 4 code along!

---
+++ Week04/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Week04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool snowing = true; 
+            bool haveCarrot=true;
+            int temp = 40; 
+            string name = "Lucas";
+
+            if((snowing && temp <=40 ) && (name !="Bob" || haveCarrot)){
+                Console.WriteLine("I am a snowman");
+
+           // string name = "Random String"; //FALSE
+          //  if ("Random String" != "Random String"){
+           //    Console.WriteLine("It worked"); 
+           // } 
+
+           // int number = 15;  //TRUE
+           // if( number >7 ){
+            //    Console.WriteLine("It worked"); 
+           // }
+
+            // int number = 27;  //TRUE
+            //  if( number <= 152 ){
+            //       Console.WriteLine("It worked");
+             // }
+
+             //   string nameTwo = "My Name Is Jordan"; //FALSE
+             //   if( nameTwo =="my name is jordan"){
+             //       Console.WriteLine("It worked");
+             //   }
+
+             //  int number = 35; //TRUE 
+             //  if( number >=  35){
+             //      Console.WriteLine("It worked");
+             //  }
+
+             //   double number = 3.1254798;  //TRUE
+             //   if (number <= 5){
+             //       Console.WriteLine("It worked");
+             //   }
+
+            //  int number = 9001; //FALSE
+             //   if(number < 9001){
+             //       Console.WriteLine("It worked");
+                }
+
+            // False  true  false  
+
+
+            }
+
+        }
+    }
+}
+
+// false, true, true, false, true, true, false 
