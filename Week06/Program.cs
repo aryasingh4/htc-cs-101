cdb9233eadafb8da6962063dec36c0b557cfc745
User <User@DEV-022783.OnBase.net>
2019-03-12 19:26:09 -0400

    factorial

---
+++ Week06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Week06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            userInput = Convert.ToInt32(Console.ReadLine()); 
+            even(userInput);
+            Console.WriteLine("Pick a number");
+        }
+
+         public static void even(int numberTwo )
+            {
+                int x = 0; 
+            if ( numberTwo  % 2 == 0 ){
+              Console.WriteLine("even");
+            }
+           
+            }
+        }
+    }
+}
